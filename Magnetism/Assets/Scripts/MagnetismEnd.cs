802f1db0a760e04a77f828fdeedb1c98d8135184
GHersik <80617931+GHersik@users.noreply.github.com>
2022-07-02 12:17:39 +0100

    CinemachineAdded
    New finishing cams, animator for cinemachine, changes to the end script.

--- Magnetism/Assets/Scripts/MagnetismEnd.cs
+++ Magnetism/Assets/Scripts/MagnetismEnd.cs
@@ -7,12 +7,14 @@ public class MagnetismEnd : MonoBehaviour
 {
     BoxCollider2D[] colliders;
     PlayableDirector timeline;
+    Animator[] animators;
     int counter = 0;
 
     void Start()
     {
         colliders = GetComponents<BoxCollider2D>();
         timeline = GetComponentInChildren<PlayableDirector>();
+        animators = GetComponentsInChildren<Animator>();
     }
 
 
@@ -20,8 +22,17 @@ public class MagnetismEnd : MonoBehaviour
     {
         if (counter < 1 && (colliders[0].IsTouchingLayers(LayerMask.GetMask("Magnetic")) && colliders[1].IsTouchingLayers(LayerMask.GetMask("Magnetic"))))
         {
-            timeline.Play();
             counter++;
+            timeline.Play();
+            animators[1].SetBool("switchToMidCam", true);
+            StartCoroutine(Wait());
+
         }
+    }
+
+    IEnumerator Wait()
+    {
+        yield return new WaitForSeconds(6);
+        animators[1].SetBool("switchToOutTowerCam", true);
     }
 }
